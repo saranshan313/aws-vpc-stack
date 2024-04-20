2e60da8d2291636a7c8309671728fb7d1f5cf25a
saranshan313 <sar.mca05@gmail.com>
2024-04-21 09:18:31 +1000

    update outputs

--- terraform/04-outputs.tf
+++ terraform/04-outputs.tf
@@ -3,6 +3,11 @@ output "network_vpc_id" {
   value       = try(aws_vpc.network_vpc.id, null)
 }
 
+output "network_vpc_cidr" {
+  description = "cidr of the VPC"
+  value       = try(local.settings.vpc_cidr_range, null)
+}
+
 output "network_public_subnets" {
   description = "Id of the public subnets"
   value       = try({ for k, v in aws_subnet.network_public_subnets : k => v.id }, null)
